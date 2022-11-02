4463614d741a630e60b7a68af8342fb936eb7000
sigmaprogrammer <114832681+iachamani@users.noreply.github.com>
2022-11-02 22:53:46 +0000

    Update linear.go

--- search/linear.go
+++ search/linear.go
@@ -1,11 +1,11 @@
 package search
 
 // Linear Simple linear search algorithm that iterates over all elements of an array in the worst case scenario
-func Linear(array []int, query int) (int, error) {
-	for i, item := range array {
-		if item == query {
-			return i, nil
+func Linear(array []int, query int, Size int) (bool, error) {
+	for i := 0; i < Size; i++ {
+		if array[i] == query {
+			return true, nil // true we found that query
 		}
 	}
-	return -1, ErrNotFound
+	return false, ErrNotFound //we didn't find it
 }
